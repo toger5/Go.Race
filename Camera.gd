a58fd193e63801bcc5e2bde04e28722d63aa22f1
Timo Kandra <toger5@hotmail.de>
2019-01-14 01:19:00 +0100

    main, project, camera, icon ..

---
+++ Camera.gd
@@ -0,0 +1,9 @@
+extends Camera
+
+var a = 2
+
+func _ready():
+	pass
+
+func _process(d):
+	pass
---
+++ quadcopter/Quadcopter.gd
@@ -0,0 +1,44 @@
+extends RigidBody
+
+# class member variables go here, for example:
+var power = 2
+var rate = 2
+
+func _ready():
+	input_getter.set_use_mouse(true)
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+func _input(e):
+	#Toggle mouse use mouse (esc -> use mouse = false, click -> use_mouse = true)
+	var mouse_button_ev : = e as InputEventMouseButton
+	if mouse_button_ev and not input_getter.use_mouse:
+		input_getter.set_use_mouse(true)
+	if e.is_action("ui_cancel"):
+		if e.pressed:
+			input_getter.set_use_mouse(false)
+func lv(v):
+	return transform.basis.xform(v)
+
+func _physics_process(delta):
+	delta = delta
+#	var joy_throttle = (Input.get_joy_axis(0,2)+0.8)/2
+#	if joy_throttle < 0:
+#		joy_throttle = 0
+#	var joy_roll = -Input.get_joy_axis(0,0)
+#	var joy_pitch = -Input.get_joy_axis(0,1)
+#	var joy_yaw = -Input.get_joy_axis(0,3)
+	var joy_throttle := input_getter.get_throttle()
+	if joy_throttle < 0.0:
+		joy_throttle = 0.0
+	var joy_roll := input_getter.get_roll()
+	var joy_pitch := input_getter.get_pitch()
+	var joy_yaw := input_getter.get_yaw()
+	input_getter.reset_speed()
+	
+	angular_velocity = Vector3()
+	apply_impulse(Vector3(),lv(Vector3(0,joy_throttle*power,0)))
+	angular_velocity = lv(Vector3(0 ,0,joy_roll*rate))
+	angular_velocity = angular_velocity + lv(Vector3(joy_pitch*rate ,0,0))
+	angular_velocity = angular_velocity + lv(Vector3(0,joy_yaw*rate,0))
+
