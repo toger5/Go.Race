708953b8ac8258a0bc7a3274fd2d75e451148f47
Timo Kandra <toger5@hotmail.de>
2019-01-27 02:31:08 +0100

    mwp

--- quadcopter/Quadcopter.gd
+++ quadcopter/Quadcopter.gd
@@ -1,14 +1,30 @@
 extends RigidBody
 
 # class member variables go here, for example:
-var power = 200.0
-var rate = 6
-var air_resistance = 0.006
+var power = 160.0
+var rate = 3.1
+var air_resistance = 0.01
+
+onready var sound = $AudioStreamPlayer
 func _ready():
+	sound.stream.loop_mode = AudioStreamSample.LOOP_PING_PONG
+	sound.playing = true
+	connect("body_entered",self,"_body_entered")
+	connect("body_exited",self,"_body_exit")
 	input_getter.set_use_mouse(true)
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+var sound_pitch = 0.0
+func _process(delta):
+	var sp = sound.pitch_scale
+	if abs(sound_pitch - sound.pitch_scale) > 0.03:
+		sp += sign(sound_pitch - sound.pitch_scale) * 0.03
+	else:
+		print("fast")
+		sound.pitch_scale = sound_pitch
+	sound.pitch_scale = max(0.01, sp)
+	print(sound.pitch_scale)
 func _input(e):
 	#Toggle mouse use mouse (esc -> use mouse = false, click -> use_mouse = true)
 	var mouse_button_ev : = e as InputEventMouseButton
@@ -19,9 +35,15 @@ func _input(e):
 			input_getter.set_use_mouse(false)
 func lv(v):
 	return transform.basis.xform(v)
-
-
-func _physics_process(delta):
+var coll = false
+func _body_entered(c):
+	print("is colliding")
+	coll = true
+func _body_exit(c):
+	if get_colliding_bodies().size() == 0:
+		coll = false
+	print("exit left: " + str(get_colliding_bodies().size()))
+func _integrate_forces(ph):
 	var joy_throttle := input_getter.get_throttle()
 	if joy_throttle < 0.0:
 		joy_throttle = 0.0
@@ -30,10 +52,27 @@ func _physics_process(delta):
 	var joy_yaw := input_getter.get_yaw()
 	
 	#apply prop impulse
-	angular_velocity = Vector3()
 	var motor_force = lv(Vector3(0,joy_throttle*power,0))
-	angular_velocity = lv(Vector3(0 ,0,-joy_roll*rate))
-	angular_velocity = angular_velocity + lv(Vector3(-joy_pitch*rate ,0,0))
-	angular_velocity = angular_velocity + lv(Vector3(0,-joy_yaw*rate,0))
+	var roll_pow = 0.6
+
+#	if abs(ph.angular_velocity.z) < abs(joy_roll*rate):
+#		ph.add_torque(lv( Vector3(0 ,0, -joy_roll*roll_pow)))
+#	if abs(ph.angular_velocity.z) > abs(joy_roll*rate):
+#		ph.add_torque(lv( Vector3(0 ,0, -sign(ph.angular_velocity.z)*roll_pow)))
+#
+#	if abs(ph.angular_velocity.y) < abs(joy_yaw*rate):
+#		ph.add_torque( lv(Vector3(0 ,-joy_yaw*roll_pow, 0)))
+#	if abs(ph.angular_velocity.y) > abs(joy_yaw*rate):
+#		ph.add_torque(lv( Vector3(0 ,-sign(ph.angular_velocity.y)*roll_pow, 0)))
+#
+#	if abs(ph.angular_velocity.x) < abs(joy_pitch*rate):
+#		ph.add_torque( lv(Vector3(-joy_pitch*roll_pow ,0, 0)))
+#	if abs(ph.angular_velocity.x) > abs(joy_pitch*rate):
+#		ph.add_torque(lv( Vector3(-sign(ph.angular_velocity.x)*roll_pow ,0, 0)))
+	ph.angular_velocity = lv(Vector3(0 ,0,-joy_roll*rate))
+	ph.angular_velocity += lv(Vector3(-joy_pitch*rate ,0,0))
+	ph.angular_velocity += lv(Vector3(0,-joy_yaw*rate,0))
 	var air_force = -linear_velocity.normalized() * linear_velocity.length_squared() * air_resistance
-	add_central_force(motor_force + air_force)
+	ph.add_central_force(motor_force + air_force)
+	sound_pitch = 0.7+motor_force.length()/40 + air_force.length() / 80
+
